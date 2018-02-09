9ad548826866c702c4de2956348347be9ed5bb87
Ahmed Ali <ahmed@Jochens-MacBook-Pro.local>
2018-02-08 23:44:12 -0500

    add some histos

--- DSelector_justin_1_analyzer.h
+++ DSelector_justin_1_analyzer.h
@@ -13,62 +13,65 @@
 
 class DSelector_justin_1_analyzer : public DSelector
 {
-	public:
-
-		DSelector_justin_1_analyzer(TTree* locTree = NULL) : DSelector(locTree){}
-		virtual ~DSelector_justin_1_analyzer(){}
-
-		void Init(TTree *tree);
-		Bool_t Process(Long64_t entry);
-
-	private:
-
-		void Get_ComboWrappers(void);
-		void Finalize(void);
-
-		// BEAM POLARIZATION INFORMATION
-		UInt_t dPreviousRunNumber;
-		bool dIsPolarizedFlag; //else is AMO
-		bool dIsPARAFlag; //else is PERP or AMO
-
-		//CREATE REACTION-SPECIFIC PARTICLE ARRAYS
-
-		//Step 0
-		DParticleComboStep* dStep0Wrapper;
-		DBeamParticle* dComboBeamWrapper;
-		DChargedTrackHypothesis* dPiMinus1Wrapper;
-		DChargedTrackHypothesis* dKPlusWrapper;
-		DChargedTrackHypothesis* dProtonWrapper;
-
-		//Step 1
-		DParticleComboStep* dStep1Wrapper;
-		DChargedTrackHypothesis* dPiMinus2Wrapper;
-		DChargedTrackHypothesis* dPiPlusWrapper;
-
-		// DEFINE YOUR HISTOGRAMS HERE
-		// EXAMPLES:
-		TH1I* dHist_MissingMassSquared;
-		TH1I* dHist_BeamEnergy;
-
-            // TOOL FOR FLAT TREE OUTPUT
-            DComboTreeHelper *dComboTreeHelper;
-
-	ClassDef(DSelector_justin_1_analyzer, 0);
+public:
+    
+    DSelector_justin_1_analyzer(TTree* locTree = NULL) : DSelector(locTree){}
+    virtual ~DSelector_justin_1_analyzer(){}
+    
+    void Init(TTree *tree);
+    Bool_t Process(Long64_t entry);
+    
+private:
+    
+    void Get_ComboWrappers(void);
+    void Finalize(void);
+    
+    // BEAM POLARIZATION INFORMATION
+    UInt_t dPreviousRunNumber;
+    bool dIsPolarizedFlag; //else is AMO
+    bool dIsPARAFlag; //else is PERP or AMO
+    
+    //CREATE REACTION-SPECIFIC PARTICLE ARRAYS
+    
+    //Step 0
+    DParticleComboStep* dStep0Wrapper;
+    DBeamParticle* dComboBeamWrapper;
+    DChargedTrackHypothesis* dPiMinus1Wrapper;
+    DChargedTrackHypothesis* dKPlusWrapper;
+    DChargedTrackHypothesis* dProtonWrapper;
+    
+    //Step 1
+    DParticleComboStep* dStep1Wrapper;
+    DChargedTrackHypothesis* dPiMinus2Wrapper;
+    DChargedTrackHypothesis* dPiPlusWrapper;
+    
+    // DEFINE YOUR HISTOGRAMS HERE
+    // EXAMPLES:
+    TH1I* dHist_MissingMassSquared;
+    TH1I* dHist_BeamEnergy;
+    TH1I* dHist_KinFitConLev;
+    TH1I* dHist_KsMass_Measured;
+    TH1I* dHist_KsMass_KinFit;
+    
+    // TOOL FOR FLAT TREE OUTPUT
+    DComboTreeHelper *dComboTreeHelper;
+    
+    ClassDef(DSelector_justin_1_analyzer, 0);
 };
 
 void DSelector_justin_1_analyzer::Get_ComboWrappers(void)
 {
-	//Step 0
-	dStep0Wrapper = dComboWrapper->Get_ParticleComboStep(0);
-	dComboBeamWrapper = static_cast<DBeamParticle*>(dStep0Wrapper->Get_InitialParticle());
-	dPiMinus1Wrapper = static_cast<DChargedTrackHypothesis*>(dStep0Wrapper->Get_FinalParticle(0));
-	dKPlusWrapper = static_cast<DChargedTrackHypothesis*>(dStep0Wrapper->Get_FinalParticle(1));
-	dProtonWrapper = static_cast<DChargedTrackHypothesis*>(dStep0Wrapper->Get_FinalParticle(3));
-
-	//Step 1
-	dStep1Wrapper = dComboWrapper->Get_ParticleComboStep(1);
-	dPiMinus2Wrapper = static_cast<DChargedTrackHypothesis*>(dStep1Wrapper->Get_FinalParticle(0));
-	dPiPlusWrapper = static_cast<DChargedTrackHypothesis*>(dStep1Wrapper->Get_FinalParticle(1));
+    //Step 0
+    dStep0Wrapper = dComboWrapper->Get_ParticleComboStep(0);
+    dComboBeamWrapper = static_cast<DBeamParticle*>(dStep0Wrapper->Get_InitialParticle());
+    dPiMinus1Wrapper = static_cast<DChargedTrackHypothesis*>(dStep0Wrapper->Get_FinalParticle(0));
+    dKPlusWrapper = static_cast<DChargedTrackHypothesis*>(dStep0Wrapper->Get_FinalParticle(1));
+    dProtonWrapper = static_cast<DChargedTrackHypothesis*>(dStep0Wrapper->Get_FinalParticle(3));
+    
+    //Step 1
+    dStep1Wrapper = dComboWrapper->Get_ParticleComboStep(1);
+    dPiMinus2Wrapper = static_cast<DChargedTrackHypothesis*>(dStep1Wrapper->Get_FinalParticle(0));
+    dPiPlusWrapper = static_cast<DChargedTrackHypothesis*>(dStep1Wrapper->Get_FinalParticle(1));
 }
 
 #endif // DSelector_justin_1_analyzer_h
