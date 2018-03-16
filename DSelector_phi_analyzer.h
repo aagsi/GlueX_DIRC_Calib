6c8770cf423eaa8051951662455d2ab424ffde5c
Ahmed Ali <ahmedali@ifarm1401.jlab.org>
2018-03-16 11:33:33 -0400

    analyzer for phi

---
+++ DSelector_phi_analyzer.h
@@ -0,0 +1,60 @@
+#ifndef DSelector_phi_analyzer_h
+#define DSelector_phi_analyzer_h
+
+#include <iostream>
+
+#include "DSelector/DSelector.h"
+#include "DSelector/DHistogramActions.h"
+#include "DSelector/DCutActions.h"
+
+#include "TH1I.h"
+#include "TH2I.h"
+
+class DSelector_phi_analyzer : public DSelector
+{
+	public:
+
+		DSelector_phi_analyzer(TTree* locTree = NULL) : DSelector(locTree){}
+		virtual ~DSelector_phi_analyzer(){}
+
+		void Init(TTree *tree);
+		Bool_t Process(Long64_t entry);
+
+	private:
+
+		void Get_ComboWrappers(void);
+		void Finalize(void);
+
+		// BEAM POLARIZATION INFORMATION
+		UInt_t dPreviousRunNumber;
+		bool dIsPolarizedFlag; //else is AMO
+		bool dIsPARAFlag; //else is PERP or AMO
+
+		//CREATE REACTION-SPECIFIC PARTICLE ARRAYS
+
+		//Step 0
+		DParticleComboStep* dStep0Wrapper;
+		DBeamParticle* dComboBeamWrapper;
+		DChargedTrackHypothesis* dKPlusWrapper;
+		DChargedTrackHypothesis* dKMinusWrapper;
+		DChargedTrackHypothesis* dProtonWrapper;
+
+		// DEFINE YOUR HISTOGRAMS HERE
+		// EXAMPLES:
+		TH1I* dHist_MissingMassSquared;
+		TH1I* dHist_BeamEnergy;
+
+	ClassDef(DSelector_phi_analyzer, 0);
+};
+
+void DSelector_phi_analyzer::Get_ComboWrappers(void)
+{
+	//Step 0
+	dStep0Wrapper = dComboWrapper->Get_ParticleComboStep(0);
+	dComboBeamWrapper = static_cast<DBeamParticle*>(dStep0Wrapper->Get_InitialParticle());
+	dKPlusWrapper = static_cast<DChargedTrackHypothesis*>(dStep0Wrapper->Get_FinalParticle(0));
+	dKMinusWrapper = static_cast<DChargedTrackHypothesis*>(dStep0Wrapper->Get_FinalParticle(1));
+	dProtonWrapper = static_cast<DChargedTrackHypothesis*>(dStep0Wrapper->Get_FinalParticle(2));
+}
+
+#endif // DSelector_phi_analyzer_h
